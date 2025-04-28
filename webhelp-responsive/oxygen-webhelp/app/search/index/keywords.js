ad95a238bd27cc62bf9fa6acfd462fcffaf1a76b
Oleksandra Zhupynska <140613069+ozhup@users.noreply.github.com>
2025-04-28 17:24:40 +0200

    Add files via upload

---
+++ webhelp-responsive/oxygen-webhelp/app/search/index/keywords.js
@@ -0,0 +1,11 @@
+var keywords=[{w:"Allgemeine",p:["p0"]},{w:"Informationen",p:["p0"]},{w:"zum",p:["p0"]},{w:"Barcode-Scanner",p:["p0"]},{w:"Context",p:["p1"]},{w:"Sensitive",p:["p1"]},{w:"Help",p:["p1"]},{w:"Erste",p:["p2"]},{w:"Einstellungen",p:["p2"]},{w:"Verwendung",p:["p3","p4"]},{w:"des",p:["p3","p4"]},{w:"Scanners",p:["p3","p4"]},{w:"im",p:["p3","p4"]},{w:"kabelgebundenen",p:["p3"]},{w:"Modus",p:["p3","p4"]},{w:"drahtlosen",p:["p4"]}];
+var ph={};
+ph["p0"]=[0, 1, 2, 3];
+ph["p1"]=[4, 5, 6];
+ph["p2"]=[7, 8];
+ph["p3"]=[9, 10, 11, 12, 13, 14];
+ph["p4"]=[9, 10, 11, 12, 15, 14];
+var keywordsInfo = {
+    keywords: keywords,
+    ph: ph
+}
---
+++ webhelp-responsive/oxygen-webhelp/app/search/index/link-to-parent.js
@@ -0,0 +1,2 @@
+/*Maps current topic to its parent: "topicIndex:parentIndex". -1 represents the map.*/
+var linkToParent = {0:-1,4:-1,5:-1,2:0};
